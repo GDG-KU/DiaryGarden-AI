0bc2a699e2b1473beedadbc2c4953d2f64ef1498
yeono1220 <yeono1220@gmail.com>
2025-12-05 00:55:04 +0900

    dd

--- train/train.py
+++ train/train.py
@@ -79,10 +79,10 @@ training_args = TrainingArguments(
     learning_rate=2e-5,
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
-    num_train_epochs=5,
+    num_train_epochs=3,
     weight_decay=0.01,
     logging_steps=10,
-    save_total_limit=2,
+    save_total_limit=1,
     load_best_model_at_end=True,
     fp16=torch.cuda.is_available(),
 )
